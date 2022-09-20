66e98a39d70e1f21ac3f41cbd4ccd85c6db46ea2
theforps <theforps@gmail.com>
2022-09-20 20:24:08 +0300

    Update 20/09/22

---
+++ Tasks/PalindromeNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+//  https://leetcode.com/problems/palindrome-number/
+*/
+
+public class PalindromeNumber
+{
+    public static bool IsPalindrome(int x)
+    {
+        string str = Convert.ToString(x);
+
+        char[] chars = str.ToCharArray();
+
+        Array.Reverse(chars);
+
+        string newStr = new string(chars);
+
+        if (newStr == str)
+            return true;
+        
+        else
+            return false;
+    }
+
+    #region test cases
+    static void Main(string[] args)
+    {
+        //will return true
+        Console.WriteLine(IsPalindrome(121));
+        //will return false
+        Console.WriteLine(IsPalindrome(-121));
+        //will return false
+        Console.WriteLine(IsPalindrome(10));
+    }
+    #endregion
+}
