eadd7cef4eecd9d882cd2cf57768f4ccde39fb99
theforps <theforps@gmail.com>
2022-09-21 11:06:08 +0300

    Update 21/09/22

---
+++ Tasks/ValidPalindrome.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/*
+//  https://leetcode.com/problems/valid-palindrome/
+*/
+
+public class ValidPalindrome
+{
+    public static bool IsPalindrome(string s)
+    {
+        char[] symbols = { ',', '.', ':', ' ', '/', '#', '_', '-', '?',
+            '!', ';', ')', '(', '`', '"', '[', ']', '@', '\'', '{', '}' };
+
+        for (int i = 0; i < symbols.Length; i++)
+            if (s.Contains(symbols[i].ToString()))
+                s = s.Replace($"{symbols[i]}", "");
+
+        s = s.ToLower();
+
+        char[] chars = s.ToCharArray();
+        Array.Reverse(chars);
+        string str2 = new string(chars);
+
+
+        if (s == str2) return true;
+        else return false;
+    }
+
+    #region test cases
+    static void Main(string[] args)
+    {
+        //will return true
+        Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+        //will return false
+        Console.WriteLine(IsPalindrome("race a car"));
+        //will return true
+        Console.WriteLine(IsPalindrome(" "));
+    }
+    #endregion
+}
