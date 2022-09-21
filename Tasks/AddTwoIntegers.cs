04708b68915716295102224f0bbdf49ac8c197f8
theforps <theforps@gmail.com>
2022-09-21 11:18:40 +0300

    Update 21/09/22

---
+++ Tasks/AddTwoIntegers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+//  https://leetcode.com/problems/add-two-integers/
+*/
+
+public class AddTwoIntegers
+{
+    public static int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    #region test cases
+    static void Main(string[] args)
+    {
+        //will return 17
+        Console.WriteLine(Sum( 12, 5 ));
+        //will return -6
+        Console.WriteLine(Sum( -10, 4 ));
+    }
+    #endregion
+}
---
+++ Tasks/ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+//  https://leetcode.com/problems/reverse-string/
+*/
+
+public class ReverseString
+{
+    public static char[] ReverseStringOne(char[] s)
+    {
+        Array.Reverse(s);
+        return s;
+    }
+
+    #region test cases
+    static void Main(string[] args)
+    {
+        //will return "o","l","l","e","h"
+        Console.WriteLine(ReverseStringOne(new char[] { 'h', 'e', 'l', 'l', 'o' }));
+        //will return "h","a","n","n","a","H"
+        Console.WriteLine(ReverseStringOne(new char[] { 'H', 'a', 'n', 'n', 'a', 'h' }));
+    }
+    #endregion
+}
