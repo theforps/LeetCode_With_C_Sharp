f2fdf22e28deab7d8f8a1a6c51e897c962f5a164
theforps <theforps@gmail.com>
2022-09-23 19:47:56 +0300

    Update 23/09/22

--- Tasks/MergeSortedArray.cs
+++ Tasks/MergeSortedArray.cs
@@ -6,6 +6,23 @@
 
 public class MergeSortedArray
 {
-    
+    public static int[] Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        if (m == 0)
+        {
+            for (int i = 0; i < nums2.Length; i++)
+                nums1[i] = nums2[i];
+        }
+        else
+        {
+            for (int i = 0; i < nums2.Length; i++)
+                nums1[i + m] = nums2[i];
+        }
+
+        Array.Sort(nums1);
+
+        return nums1;
+    }
+
 }
 
