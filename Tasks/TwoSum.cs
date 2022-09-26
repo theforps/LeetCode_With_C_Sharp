4eca44d01843a5e078d645d6725365f6ee6de7dd
theforps <theforps@gmail.com>
2022-09-27 00:08:49 +0300

    Update 27/09/22

---
+++ Tasks/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+namespace LeetCode.Tasks
+{
+    public class TwoSum
+    {
+        public static int[] twoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 1; j < nums.Length; j++)
+                    if ((nums[i] + nums[j]) == target && (i != j))
+                        return new int[] { i, j };
+            return null;
+        }
+
+        #region test cases
+        static void Main(string[] args)
+        {
+            //will return [0,1]
+            foreach (var x in twoSum(new int[] { 2, 7, 11, 15 }, 9))
+                Console.Write(x + "");
+        }
+        #endregion
+    }
+}
