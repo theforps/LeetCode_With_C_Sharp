73ec152b2776650a1cc391f7913d71cb4de0ebc8
theforps <theforps@gmail.com>
2022-09-27 00:12:11 +0300

    Update 27/09/22

--- Tasks/TwoSum.cs
+++ Tasks/TwoSum.cs
@@ -23,6 +23,12 @@ namespace LeetCode.Tasks
             //will return [0,1]
             foreach (var x in twoSum(new int[] { 2, 7, 11, 15 }, 9))
                 Console.Write(x + "");
+            //will return [1,2]
+            foreach (var x in twoSum(new int[] { 3, 2, 4 }, 6))
+                Console.Write(x + "");
+            //will return [0,1]
+            foreach (var x in twoSum(new int[] { 2, 7, 11, 15 }, 9))
+                Console.Write(x + "");
         }
         #endregion
     }
