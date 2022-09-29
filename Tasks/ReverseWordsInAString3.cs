b62e016d998f9709c322051bf7a99d6f16373c1d
theforps <theforps@gmail.com>
2022-09-29 20:05:50 +0300

    Update 29/09/22

--- Tasks/ReverseWordsInAString3.cs
+++ Tasks/ReverseWordsInAString3.cs
@@ -8,6 +8,32 @@ namespace LeetCode.Tasks
 {
     public class ReverseWordsInAString3
     {
+        public static string ReverseWords(string s)
+        {
+            string[] strings = s.Split(' ');
+            s = "";
 
+            for (int i = 0; i < strings.Length; i++)
+            {
+                char[] chars = strings[i].ToCharArray();
+                Array.Reverse(chars);
+                strings[i] = new string(chars);
+                s += strings[i] + " ";
+            }
+
+            s = s.Trim();
+
+            return s;
+        }
+
+        #region test cases
+        static void Main(string[] args)
+        {
+            //will return "o","l","l","e","h"
+            Console.WriteLine(ReverseStringOne(new char[] { 'h', 'e', 'l', 'l', 'o' }));
+            //will return "h","a","n","n","a","H"
+            Console.WriteLine(ReverseStringOne(new char[] { 'H', 'a', 'n', 'n', 'a', 'h' }));
+        }
+        #endregion
     }
 }
