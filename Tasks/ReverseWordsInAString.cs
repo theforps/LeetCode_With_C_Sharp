3de1e446e17f5ccf230642ab9ecabc732ba9bdb4
theforps <theforps@gmail.com>
2022-09-30 23:55:12 +0300

    Update 30/09/22

--- Tasks/ReverseWordsInAString.cs
+++ Tasks/ReverseWordsInAString.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode.Tasks
 {
     public class ReverseWordsInAString
     {
-        public string ReverseWords(string s)
+        public static string ReverseWords(string s)
         {
             string[] strArray = s.Split(' ');
 
