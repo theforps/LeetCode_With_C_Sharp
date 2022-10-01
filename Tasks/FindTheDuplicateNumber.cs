14f10f3f067f7faed944eff2c0330d1a345a9da9
theforps <theforps@gmail.com>
2022-10-01 12:56:32 +0300

    Update 01/10/22

--- Tasks/FindTheDuplicateNumber.cs
+++ Tasks/FindTheDuplicateNumber.cs
@@ -8,6 +8,25 @@ namespace LeetCode.Tasks
 {
     public class FindTheDuplicateNumber
     {
+        public int FindDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
 
+            for (int i = 1; i < nums.Length; i++)
+                if (nums[i] == nums[i - 1]) return nums[i];
+
+            return 0;
+
+            // variant 2
+            /* 
+            for (ushort i = 0; i < nums.Length; i++)
+                for (ushort j = ushort.Parse((nums.Length-1).ToString()); j > 0; j--)
+                    if (i != j && nums[i] == nums[j]) return nums[i];
+
+            return 0;
+            */
+
+
+        }
     }
 }
