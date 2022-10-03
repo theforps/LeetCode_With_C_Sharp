7e6875a60fa19d946a8c578e3dfe57e8f800d7e6
theforps <theforps@gmail.com>
2022-10-03 22:47:22 +0300

    Update 03/10/22

--- Tasks/Medium/ValidateIPAdress.cs
+++ Tasks/Medium/ValidateIPAdress.cs
@@ -9,7 +9,7 @@ namespace LeetCode.Tasks.Medium
 {
     public class ValidateIPAdress
     {
-        public string ValidIPAddress(string queryIP)
+        public static string ValidIPAddress(string queryIP)
         {
             char[] goodChars = { 'a', 'b', 'c', 'd', 'e', 'f' , '0', '1',
             '2', '3', '4','5','6','7','8','9'};
