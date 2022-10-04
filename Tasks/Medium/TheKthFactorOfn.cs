58d1767c97bb9754753006095f4a3a224f245827
theforps <theforps@gmail.com>
2022-10-04 19:41:13 +0300

    Update 04/10/22

--- Tasks/Medium/TheKthFactorOfn.cs
+++ Tasks/Medium/TheKthFactorOfn.cs
@@ -23,5 +23,17 @@ namespace LeetCode.Tasks.Medium
             }
             return -1;
         }
+
+        #region test cases
+        static void Main(string[] args)
+        {
+            //will return "IPv4"
+            Console.WriteLine(ValidIPAddress("172.16.254.1"));
+            //will return "IPv6"
+            Console.WriteLine(ValidIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"));
+            //will return "Neither"
+            Console.WriteLine(ValidIPAddress("256.256.256.256"));
+        }
+        #endregion
     }
 }
