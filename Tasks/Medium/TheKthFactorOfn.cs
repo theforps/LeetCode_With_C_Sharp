d98564313b94a47b5d9ea5bd043a940a3eaeab1b
theforps <theforps@gmail.com>
2022-10-04 19:40:47 +0300

    Update 04/10/22

--- Tasks/Medium/TheKthFactorOfn.cs
+++ Tasks/Medium/TheKthFactorOfn.cs
@@ -8,6 +8,20 @@ namespace LeetCode.Tasks.Medium
 {
     public class TheKthFactorOfn
     {
+        public int KthFactor(int n, int k)
+        {
+            var list = new List<int>();
 
+            if (n >= k)
+            {
+                for (int i = 1; i < n + 1; i++)
+                {
+                    if (n % i == 0)
+                        list.Add(i);
+                }
+                if (list.Count >= k) return list[k - 1];
+            }
+            return -1;
+        }
     }
 }
