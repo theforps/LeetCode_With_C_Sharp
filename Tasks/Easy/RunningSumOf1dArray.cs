61a5edb32689502db37ec0e5726edf3b5ff7526f
theforps <theforps@gmail.com>
2022-10-05 22:14:25 +0300

    Update 05/10/22

--- Tasks/Easy/RunningSumOf1dArray.cs
+++ Tasks/Easy/RunningSumOf1dArray.cs
@@ -8,6 +8,19 @@ namespace LeetCode.Tasks.Easy
 {
     public class RunningSumOf1dArray
     {
+        public int[] RunningSum(int[] nums)
+        {
+            int result = 0;
+            int[] ints = new int[nums.Length];
 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result = 0;
+                for (int j = 0; j < i + 1; j++)
+                    result += nums[j];
+                ints[i] = result;
+            }
+            return ints;
+        }
     }
 }
