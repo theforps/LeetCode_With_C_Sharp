70f4afbbbaf3e8ce838fcf8abc38d6effa7a307d
theforps <theforps@gmail.com>
2022-10-05 22:14:41 +0300

    Update 05/10/22

--- Tasks/Easy/RunningSumOf1dArray.cs
+++ Tasks/Easy/RunningSumOf1dArray.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode.Tasks.Easy
+﻿namespace LeetCode.Tasks.Easy
 {
     public class RunningSumOf1dArray
     {
