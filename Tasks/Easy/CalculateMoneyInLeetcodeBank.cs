482acdc77174258409046eb37de97f7c93bf922a
theforps <theforps@gmail.com>
2022-10-06 19:35:33 +0300

    Update 06/10/22

--- Tasks/Easy/CalculateMoneyInLeetcodeBank.cs
+++ Tasks/Easy/CalculateMoneyInLeetcodeBank.cs
@@ -8,6 +8,25 @@ namespace LeetCode.Tasks.Easy
 {
     public class CalculateMoneyInLeetcodeBank
     {
+        public int TotalMoney(int n)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int sum = 0;
 
+            for (int i = 0; i < n / 7; i++)
+                for (int j = 0; j < 7; j++)
+                {
+                    sum += nums[j];
+                    nums[j] += 1;
+                }
+
+            for (int j = 0; j < n % 7; j++)
+            {
+                sum += nums[j];
+                nums[j] += 1;
+            }
+
+            return sum;
+        }
     }
 }
