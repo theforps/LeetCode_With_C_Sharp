b8f789d5aa9a8d3d748409c63a2dcf577c985c23
theforps <theforps@gmail.com>
2024-05-06 17:14:39 +0300

    :)

---
+++ Tasks/Easy/MajorityElement.cs
@@ -0,0 +1,36 @@
+﻿/*
+//  https://leetcode.com/problems/majority-element/
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks.Easy
+{
+    public class MajorityElement
+    {
+        public MajorityElement(int[] nums)
+        {
+            Console.WriteLine(MajorityElement_(nums));
+        }
+        
+        private int MajorityElement_(int[] nums)
+        {
+            HashSet<int> hashSet = new HashSet<int>(nums);
+
+            foreach (var x in hashSet)
+            {
+                int count = nums.Where(j => j == x).Count();
+                
+                if (nums.Length / 2 < count)
+                {
+                    return x;
+                }
+            }
+            
+            
+            return 0;
+        }
+    }
+}
