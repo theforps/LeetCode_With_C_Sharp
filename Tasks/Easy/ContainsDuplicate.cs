ef79f43b510e5c17dc27cfa90b000a1b6c237cc2
theforps <theforps@gmail.com>
2024-05-12 20:01:54 +0300

    :)

---
+++ Tasks/Easy/ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿/*
+//  https://leetcode.com/problems/contains-duplicate/
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tasks.Easy
+{
+    public class ContainsDuplicate
+    {
+        public ContainsDuplicate(int[] nums)
+        {
+            Console.WriteLine(ContainsDuplicates(nums));
+        }
+        
+        private bool ContainsDuplicates(int[] nums)
+        {
+            var hash = new HashSet<int>(nums);
+            
+            return nums.Length != hash.Count;
+        }
+    }
+}
---
+++ Tasks/Easy/PowerOfFour.cs
@@ -0,0 +1,34 @@
+﻿/*
+//  https://leetcode.com/problems/power-of-four/submissions/
+*/
+
+using System;
+
+namespace LeetCode.Tasks.Easy
+{
+    public class PowerOfFour
+    {
+        public PowerOfFour(int n)
+        {
+            Console.WriteLine(IsPowerOfFour(n));
+        }
+        
+        public bool IsPowerOfFour(int n)
+        {
+            if (n == 1)
+                return true;
+            else if (n <= 0)
+                return false;
+
+            while (n != 1)
+            {
+                if (n % 4 > 0)
+                    return false;
+
+                n /= 4;
+            }
+
+            return true;
+        }
+    }
+}
